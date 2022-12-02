1ab6a0de2a7e5c9d323c826888a8a77e47ed06c1
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2022-12-01 20:18:03 -0500

    class exercice made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+
+  ngOnInit() { }
+
   title = 'sesion11';
+  form: FormGroup;
+  resultado: Number = 0;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      ingreso: ['', Validators.required],
+      egreso: ['', Validators.required]
+    })
+  }
+  calcular() {
+    console.log(this.form.controls);
+    // console.log(this.form.value);
+    this.resultado = parseInt(this.form.value.ingreso) - parseInt(this.form.value.egreso)
+  }
 }
